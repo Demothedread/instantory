cf3b3d5e18584d615126debad52e69bdc13efdde
Demothedread <demo.dreadstar@gmail.com>
2024-12-24 17:41:11 -0800

    vercel lol

--- frontend/DocumentSearch.js
+++ frontend/DocumentSearch.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import config from '../config';
+import config from './src/config';
 import './DocumentSearch.css';
 
 function DocumentSearch() {
