7440cc6ab051a6511d2c5f1afd5e8c6957e47b3d
Demothedread <demo.dreadstar@gmail.com>
2025-01-20 19:37:02 -0800

    fixed misaligned blurbs.css & aligned requirement.txt

--- frontend/src/scripts/blurbcarousel.js
+++ frontend/src/scripts/blurbcarousel.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../styles/blurb.css";
+import "./styles/blurb.css";
 import clickandone from "../assets/clickandone.png";
 import yippeekiAI from "../assets/yippeekiAI.png"; 
 
