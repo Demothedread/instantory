0221d407f3d09b9fb620e4e419597e1427b3c44b
Demothedread <demo.dreadstar@gmail.com>
2025-02-07 17:39:07 -0800

    rococo puffs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import './index.css'; 
-import { createRoot } from 'react-dom';
-import { AuthProvider } from "./contexts/AuthContext"; // Ensure this import is correct
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import App from './App';
+import { AuthProvider } from "./contexts/AuthContext";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const root = createRoot(document.getElementById("root"));
+root.render(
   <AuthProvider>
     <App />
   </AuthProvider>
