475cecbbbc88d4d57d4c82cf48767cac1fede7e5
demothedread <demo.dreadstar@gmail.com>
2025-06-12 17:58:44 -0700

    fixed import in landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../contexts/auth';
+import { AuthContext } from '../contexts/auth';
 import { GoogleLogin } from '@react-oauth/google';
 import { css } from '@emotion/react';
 import { colors } from '../../styles/theme/colors';
